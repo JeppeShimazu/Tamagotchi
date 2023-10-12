d1178d8f0e605f5563fd3351e684552f8151944b
Jesper Asplund <jesper.asplund@elev.ga.ntig.se>
2023-10-12 14:39:47 +0200

    Updated commit

--- Tamagochi/Program.cs
+++ Tamagochi/Program.cs
@@ -9,7 +9,40 @@ myTiger.name = Console.ReadLine();
 
 Console.WriteLine($",{myTiger.name}!?! Wow vilket bra namn för en Tiger");
 
-while ()
+while (myTiger.GetLiving())
 {
+  Console.Clear();
+  myTiger.PrintStats();
+  Console.WriteLine($"Vad vill du göra med {myTiger.name}?");
+  Console.WriteLine($"1. Lär {myTiger.name} något nytt!");
+  Console.WriteLine($"2. Prata med {myTiger.name}!");
+  Console.WriteLine($"3. Mata {myTiger.name}!");
+  Console.WriteLine("4. Gör ingenting alls");
 
+string doWhat = Console.ReadLine();
+
+  if (doWhat == "1")
+  {
+    Console.WriteLine($"Vilket ord vill du lära {myTiger.name}?");
+    string word = Console.ReadLine();
+    myTiger.Lär(word);
+  }
+  if (doWhat == "2")
+  {
+    myTiger.Hej();
+  }
+  if (doWhat == "3")
+  {
+    myTiger.Mata();
+  }
+  else
+  {
+    Console.WriteLine("Gör ingenting.");
+  }
+  myTiger.Tick();
+  
 }
+
+Console.WriteLine($"NEEEEEEEJ! {myTiger.name} har dött!!!!!!");
+Console.WriteLine("Klicka ENTER för att lämna... :(");
+Console.ReadLine();
--- Tamagochi/TamagotchiClasses.cs
+++ Tamagochi/TamagotchiClasses.cs
@@ -16,4 +16,55 @@ public Tiger()
 generator = new Random();
 isLiving = true;
 }
+
+public void Mata()
+{
+    Console.WriteLine($"Du matar [{name}], din tiger är inte lika hungrig längre");
+    hunger -= 2;
+    if (hunger < 0)
+    {
+        hunger = 0;
+    }
 }
+
+public void Hej()
+{
+    int wordNum = generator.Next(words.Count);
+    Console.WriteLine($"[{name}] says: {words[wordNum]}");
+    ReduceBoredom();
+}
+
+public void Lär(string word)
+{
+    Console.WriteLine($" [{name}] lär sig: {word}");
+    words.Add(word);
+    ReduceBoredom();
+}
+
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+  }
+public bool GetLiving(){
+    if (isLiving)
+    {   
+        return true;
+    }
+    return false;
+}
+
+public void ReduceBoredom()
+  {
+    Console.WriteLine($"[{name}] Är nu mindre uttråkad!!!");
+
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
+
+
+}
+
